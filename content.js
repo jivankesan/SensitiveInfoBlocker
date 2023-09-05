d2d5e74891fa283767948f13c6928032daea8eb1
JivanKesan <jkesan28@gmail.com>
2023-09-04 20:27:08 -0400

    Both the enter and send button actions trigger the sensitive data check, however I seem to have broken normal functionality of those buttons...

--- content.js
+++ content.js
@@ -1,18 +1,38 @@
 console.log("cs loaded");
 
 const messageBox = document.querySelector("#prompt-textarea");
+const sendButton = document.querySelector('[data-testid="send-button"]');
 
-if (messageBox) {
+const checkAndSend = () => {
+    chrome.runtime.sendMessage({text: messageBox.value}, function(response) {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+            return;
+        }
+        if (!response.allow){
+            alert("Sensitive information detected. Message not sent");
+        }
+        else{
+            sendButton.click();
+        }
+    });
+};
+
+if (messageBox && sendButton) {
+
     messageBox.addEventListener('keydown', function(event) {
         if (event.key === "Enter") {
             event.preventDefault();
-            chrome.runtime.sendMessage({text: messageBox.value}, function(response) {
-                if (!response.allow) {
-                    alert('Sensitive information detected. Message not sent');
-                }
-            });
+            event.stopPropagation();
+            checkAndSend();
         }
     });
+
+    sendButton.addEventListener("click", function(event){
+        event.preventDefault();
+        event.stopPropagation();
+        checkAndSend();
+    })
 } else {
     console.log("Element #prompt-textarea not found");
 }
